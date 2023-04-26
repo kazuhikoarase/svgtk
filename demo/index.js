96c1fe998d80adbd0e7eee3b50f6aa35c0fb3839
kazuhikoarase <kazuhiko.arase@gmail.com>
2023-04-27 01:48:55 +0900

    commit

--- demo/index.js
+++ demo/index.js
@@ -381,6 +381,179 @@ window.addEventListener('load', function() {
     var h = 400;
     var $s = createStage(w, h);
     var $base = $('g').attrs({
+      transform : $tb().translate(w / 2 - 190, h / 2 - 150).build() });
+    $s.append($base);
+
+    var drawPie = function(opts) {
+      opts = svgtk.extend({
+        r1 : 100, r2 : 200, r3 : 10, r4 : 10,
+        w : 10, a : Math.PI / 4
+      }, opts);
+      var $tmp = $('g').attrs({ fill : 'none',
+        stroke : '#000', 'stroke-width' : '0.5' });
+      $base.append($tmp);
+
+      var l = function(a) {
+        var rx = opts.r2 * Math.cos(a);
+        var ry = opts.r2 * Math.sin(a);
+        var wx = opts.w / 2 * Math.cos(a + Math.PI / 2);
+        var wy = opts.w / 2 * Math.sin(a + Math.PI / 2);
+        $tmp.append($('path').attrs({ d :
+          $pb().moveTo(0, 0).lineTo(rx, ry).build() }) );
+        $tmp.append($('path').attrs({ d :
+          $pb().moveTo(-wx, -wy).lineTo(rx - wx, ry - wy).build() }) );
+        $tmp.append($('path').attrs({ d :
+          $pb().moveTo(wx, wy).lineTo(rx + wx, ry + wy).build() }) );
+      };
+      l(0);
+      l(opts.a);
+
+      var arcs = [];
+
+      !function() {
+        var a = Math.asin( (opts.w / 2 + opts.r3) / (opts.r1 + opts.r3) );
+        var x = opts.r1 * Math.cos(a);
+        var y = opts.r1 * Math.sin(a);
+        var t = Math.atan2(y, x);
+        arcs.push({ order : 0, cx : 0, cy : 0,
+          r : opts.r1, t0 : opts.a - t, t1 : t });
+      }();
+
+      !function() {
+        var dt;
+        !function(a) {
+          var x = opts.r1 * Math.cos(a);
+          var y = opts.r1 * Math.sin(a);
+          var cx = (opts.r1 + opts.r3) * Math.cos(a);
+          var cy = (opts.r1 + opts.r3) * Math.sin(a);
+          var t0 = Math.atan2(y - cy, x - cx);
+          var t1 = Math.atan2(opts.w / 2 - cy, 0);
+          arcs.push({ order : 1, cx : cx, cy : cy,
+            r : opts.r3, t0 : t0, t1 : t1 });
+          dt = t1 - t0;
+        }(Math.asin( (opts.w / 2 + opts.r3) / (opts.r1 + opts.r3) ) );
+        !function(a) {
+          var x = opts.r1 * Math.cos(a);
+          var y = opts.r1 * Math.sin(a);
+          var cx = (opts.r1 + opts.r3) * Math.cos(a);
+          var cy = (opts.r1 + opts.r3) * Math.sin(a);
+          var t0 = Math.atan2(y - cy, x - cx);
+          var t1 = t0 - dt;
+          arcs.push({ order : 5, cx : cx, cy : cy,
+            r : opts.r3, t0 : t1, t1 : t0 });
+        }(opts.a - Math.asin( (opts.w / 2 + opts.r3) / (opts.r1 + opts.r3) ) );
+      }();
+
+      !function() {
+        var a = Math.asin( (opts.w / 2 + opts.r4) / (opts.r2 - opts.r4) );
+        var x = opts.r2 * Math.cos(a);
+        var y = opts.r2 * Math.sin(a);
+        var t = Math.atan2(y, x);
+        arcs.push({ order : 3, cx : 0, cy : 0,
+          r : opts.r2, t0 : t, t1 : opts.a - t });
+      }();
+
+      !function() {
+        var dt;
+        !function(a) {
+          var x = opts.r2 * Math.cos(a);
+          var y = opts.r2 * Math.sin(a);
+          var cx = (opts.r2 - opts.r4) * Math.cos(a);
+          var cy = (opts.r2 - opts.r4) * Math.sin(a);
+          var t0 = Math.atan2(opts.w / 2 - cy, 0);
+          var t1 = Math.atan2(y - cy, x - cx);
+          arcs.push({ order : 2, cx : cx, cy : cy,
+            r : opts.r4, t0 : t0, t1 : t1 });
+          dt = t1 - t0;
+        }(Math.asin( (opts.w / 2 + opts.r4) / (opts.r2 - opts.r4) ) );
+        !function(a) {
+          var x = opts.r2 * Math.cos(a);
+          var y = opts.r2 * Math.sin(a);
+          var cx = (opts.r2 - opts.r4) * Math.cos(a);
+          var cy = (opts.r2 - opts.r4) * Math.sin(a);
+          var t1 = Math.atan2(y - cy, x - cx);
+          var t0 = t1 + dt;
+          arcs.push({ order : 4, cx : cx, cy : cy,
+            r : opts.r4, t0 : t1, t1 : t0 });
+        }(opts.a - Math.asin( (opts.w / 2 + opts.r4) / (opts.r2 - opts.r4) ) );
+      }();
+
+      arcs.sort(function(a1, a2) {
+        return a1.order < a2.order ? -1 : 1;
+      });
+
+      !function() {
+        var tr1 = 8;
+        var tr2 = 5;
+        arcs.forEach(function(arc, i) {
+          var color = sc1;
+          $tmp.append($('path').attrs({ d :
+            $pb().moveTo(arc.cx, arc.cy).lineTo(
+              arc.cx + arc.r * Math.cos(arc.t0) ,
+              arc.cy + arc.r * Math.sin(arc.t0) ).build() }) );
+          $tmp.append($('path').attrs({ d :
+            $pb().moveTo(arc.cx, arc.cy).lineTo(
+              arc.cx + arc.r * Math.cos(arc.t1) ,
+              arc.cy + arc.r * Math.sin(arc.t1) ).build() }) );
+          $tmp.append($('circle').attrs({
+            cx : arc.cx, cy : arc.cy, r : arc.r }) );
+          $tmp.append($('circle').attrs({
+            cx : arc.cx + arc.r * Math.cos(arc.t0),
+            cy : arc.cy + arc.r * Math.sin(arc.t0), r : tr1,
+            fill : 'none', stroke : color }) );
+          $tmp.append($('circle').attrs({
+            cx : arc.cx + arc.r * Math.cos(arc.t1),
+            cy : arc.cy + arc.r * Math.sin(arc.t1), r : tr2,
+            fill : color, stroke : 'none' }) );
+        });
+      }();
+
+      !function() {
+        var pb = $pb();
+        arcs.forEach(function(arc) {
+          var fn =function(t) {
+            return [
+              arc.r * Math.cos(t) + arc.cx,
+              arc.r * Math.sin(t) + arc.cy
+            ];
+          };
+          var d = $math.getQuadPoints({ fn : fn,
+            min : arc.t0, max : arc.t1, n : 4, dt : 0.05 });
+          for (var i = 0; i < d.length; i += 1) {
+            if (i == 0) {
+              if (arc.order == 0) {
+                pb.moveTo(d[i][0], d[i][1]);
+              } else if (arc.order == 2) {
+                pb.lineTo(d[i][0], d[i][1]);
+              } else if (arc.order == 5) {
+                pb.lineTo(d[i][0], d[i][1]);
+              }
+            } else {
+              pb.quadTo(d[i][0], d[i][1], d[i][2], d[i][3]);
+            }
+          }
+        });
+        pb.close();
+        var $path = $('path').attrs({
+          d : pb.build(), fill : 'none', stroke : sc2 });
+        $base.append($path);
+      }();
+    };
+
+    drawPie({
+      r1 : 200, r2 : 420, r3 : 40, r4 : 60,
+      w : 80, a : Math.PI / 3
+    });
+
+    $(document.body).append($s);
+  }();
+
+  !function() {
+
+    var w = 600;
+    var h = 400;
+    var $s = createStage(w, h);
+    var $base = $('g').attrs({
       transform : $tb().translate(w / 2, h / 2).build() });
     $s.append($base);
 
