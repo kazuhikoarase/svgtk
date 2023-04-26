d53c650006b45c4cb39afde2d507b676afef3eac
kazuhikoarase <kazuhiko.arase@gmail.com>
2023-04-27 06:44:06 +0900

    commit

--- src/gear-util.js
+++ src/gear-util.js
@@ -177,8 +177,123 @@ export var gearUtil = function() {
     };
   };
 
+  var createPie = function(opts) {
+
+    opts = svgtk.extend({
+      r1 : 100, r2 : 200, r3 : 10, r4 : 10,
+      w : 10, a : Math.PI / 4, offsetAngle : 0
+    }, opts);
+
+    var arcs = [];
+
+    !function() {
+      var a = Math.asin( (opts.w / 2 + opts.r3) / (opts.r1 + opts.r3) );
+      var x = opts.r1 * Math.cos(a);
+      var y = opts.r1 * Math.sin(a);
+      var t = Math.atan2(y, x);
+      arcs.push({ order : 0, cx : 0, cy : 0,
+        r : opts.r1, t0 : opts.a - t, t1 : t });
+    }();
+
+    !function() {
+      var dt;
+      !function(a) {
+        var x = opts.r1 * Math.cos(a);
+        var y = opts.r1 * Math.sin(a);
+        var cx = (opts.r1 + opts.r3) * Math.cos(a);
+        var cy = (opts.r1 + opts.r3) * Math.sin(a);
+        var t0 = Math.atan2(y - cy, x - cx);
+        var t1 = Math.atan2(opts.w / 2 - cy, 0);
+        arcs.push({ order : 1, cx : cx, cy : cy,
+          r : opts.r3, t0 : t0, t1 : t1 });
+        dt = t1 - t0;
+      }(Math.asin( (opts.w / 2 + opts.r3) / (opts.r1 + opts.r3) ) );
+      !function(a) {
+        var x = opts.r1 * Math.cos(a);
+        var y = opts.r1 * Math.sin(a);
+        var cx = (opts.r1 + opts.r3) * Math.cos(a);
+        var cy = (opts.r1 + opts.r3) * Math.sin(a);
+        var t0 = Math.atan2(y - cy, x - cx);
+        var t1 = t0 - dt;
+        arcs.push({ order : 5, cx : cx, cy : cy,
+          r : opts.r3, t0 : t1, t1 : t0 });
+      }(opts.a - Math.asin( (opts.w / 2 + opts.r3) / (opts.r1 + opts.r3) ) );
+    }();
+
+    !function() {
+      var a = Math.asin( (opts.w / 2 + opts.r4) / (opts.r2 - opts.r4) );
+      var x = opts.r2 * Math.cos(a);
+      var y = opts.r2 * Math.sin(a);
+      var t = Math.atan2(y, x);
+      arcs.push({ order : 3, cx : 0, cy : 0,
+        r : opts.r2, t0 : t, t1 : opts.a - t });
+    }();
+
+    !function() {
+      var dt;
+      !function(a) {
+        var x = opts.r2 * Math.cos(a);
+        var y = opts.r2 * Math.sin(a);
+        var cx = (opts.r2 - opts.r4) * Math.cos(a);
+        var cy = (opts.r2 - opts.r4) * Math.sin(a);
+        var t0 = Math.atan2(opts.w / 2 - cy, 0);
+        var t1 = Math.atan2(y - cy, x - cx);
+        arcs.push({ order : 2, cx : cx, cy : cy,
+          r : opts.r4, t0 : t0, t1 : t1 });
+        dt = t1 - t0;
+      }(Math.asin( (opts.w / 2 + opts.r4) / (opts.r2 - opts.r4) ) );
+      !function(a) {
+        var x = opts.r2 * Math.cos(a);
+        var y = opts.r2 * Math.sin(a);
+        var cx = (opts.r2 - opts.r4) * Math.cos(a);
+        var cy = (opts.r2 - opts.r4) * Math.sin(a);
+        var t1 = Math.atan2(y - cy, x - cx);
+        var t0 = t1 + dt;
+        arcs.push({ order : 4, cx : cx, cy : cy,
+          r : opts.r4, t0 : t1, t1 : t0 });
+      }(opts.a - Math.asin( (opts.w / 2 + opts.r4) / (opts.r2 - opts.r4) ) );
+    }();
+
+    arcs.sort(function(a1, a2) {
+      return a1.order < a2.order ? -1 : 1;
+    });
+
+    var mat = $math.mat4().rotateZ(opts.offsetAngle);
+
+    var pb = $pb();
+    arcs.forEach(function(arc) {
+      var fn =function(t) {
+        return [
+          arc.r * Math.cos(t) + arc.cx,
+          arc.r * Math.sin(t) + arc.cy
+        ];
+      };
+      var d = $math.getQuadPoints({ fn : fn,
+        min : arc.t0, max : arc.t1, n : 4, dt : 0.05 });
+      for (var i = 0; i < d.length; i += 1) {
+        d[i] = mat.transform([d[i][0], d[i][1]]).
+          concat(mat.transform([d[i][2], d[i][3]]) );
+        if (i == 0) {
+          if (arc.order == 0) {
+            pb.moveTo(d[i][0], d[i][1]);
+          } else if (arc.order == 2) {
+            pb.lineTo(d[i][0], d[i][1]);
+          } else if (arc.order == 5) {
+            pb.lineTo(d[i][0], d[i][1]);
+          }
+        } else {
+          pb.quadTo(d[i][0], d[i][1], d[i][2], d[i][3]);
+        }
+      }
+    });
+    pb.close();
+
+    return { path : pb.build() };
+  };
+
   return {
     createGear : createGear,
-    createInnerGear : createInnerGear
+    createInnerGear : createInnerGear,
+    createPie : createPie
   };
 }();
